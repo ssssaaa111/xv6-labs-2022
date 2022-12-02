23a4e5137dd581865c93ed7253ec33e344b78f44
charming <4646968841@qq.com>
2022-12-02 11:29:54 +0000

    start xargs

---
+++ user/xargs.c
@@ -0,0 +1,58 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+#include "kernel/param.h"
+
+#define STDIN_FILENO 0
+#define MAXLINE 1024
+
+int main(int argc, char **argv)
+{
+    char line[MAXLINE];
+    char *params[MAXARG];
+    int n = 0;
+    int args_index = 0;
+    int i;
+    char *cmd = argv[1];
+    for (int i = 0; i < argc; i++)
+    {
+        params[i] = argv[i + 1];
+    }
+    // printf("size of char * is %d\n", sizeof(line));
+    // printf("size of char * is %d", (int)'\0');
+    int pid = fork();
+    
+    
+    if (pid == 0)
+    {   
+
+        while ((n = read(STDIN_FILENO, line, MAXLINE)) > 0)
+        {
+
+            printf("line is:%s \n", line);
+            params[args_index++] = cmd;
+            char *arg = (char*) malloc(sizeof(line));
+            int index = 0;
+            for (i = 0; i < n; i++)
+            {
+                if (line[i] == ' ' || line[i] == '\n')
+                {
+                    arg[index] = 0;
+                    params[args_index++] = arg;
+                    index = 0;
+                    arg = (char*) malloc(sizeof(line));
+                }
+                else arg[index++] = line[i];
+            }
+            arg[index] = 0;
+            params[args_index] = 0;
+            args_index = 0;
+            exec(cmd, params);
+        }
+    }
+    else
+    {
+        wait((int*)0);
+    }
+    return 0;
+}
