dc4bfea9ce30f818cdf1853fc206d0bb88751dbe
charming <464968841@qq.com>
2023-02-05 02:34:57 -0800

    pgtable test start code

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -78,10 +78,35 @@ sys_pgaccess(void)
   uint64 buf_addr;
   int n;
   uint64 bit_addr;
+  int abits = 0;
   argaddr(0, &buf_addr);
   argint(1, &n);
-  argint(2, &bit_addr);
+  argaddr(2, &bit_addr);
+  if (n > 32)
+  {
+    printf("can't scan for more than 32 pages");
+    return -1;
+  }
   
+  struct proc *p = myproc();
+  pagetable_t pagetable = p->pagetable;
+  vmprint(pagetable);
+  for (int i = 0; i< n; i++)
+  {
+    pte_t * p = walk(pagetable, buf_addr+i*4096, 0);
+    if (p)
+    {
+      if (*p | PTE_A)
+      {
+        abits = abits | (1<<i);
+        // clean the access bit
+        *p = *p & (~PTE_A);
+      }
+    }
+  }
+  printf("kerner abit %d \n", abits);
+
+  copyout(p->pagetable, bit_addr, (char *)&abits, 4);
   return 0;
 }
 #endif
